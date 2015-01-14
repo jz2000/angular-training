7aa399ffb44789607907ec33ead0f585737fc6e2
Sergey Zheznyakovskiy <Sergey_Zheznyakovskiy@epam.com>
2015-01-14 23:08:43 +0400

    Directives for lists of users and messages

--- app/app.js
+++ app/app.js
@@ -1,13 +1,27 @@
 (function () {
     "use strict";
-    angular.module("app", ["ngRoute"]);
+    angular.module("app", [
+        "ngRoute", 
+        "chatApp.controllers",
+        "chatApp.directives"
+    ]);
 
     angular.module("app").config(function ($routeProvider) {
         $routeProvider.otherwise({ redirectTo: "/home" });
 
         $routeProvider.when('/home', {
-            templateUrl: 'templates/phone/home.html',
+            templateUrl: 'templates/phone/chat.html',
             controller: 'HomeController'
+        });
+
+        $routeProvider.when('/users', {
+            templateUrl: 'templates/chat/users.html',
+            controller: 'UsersController'
+        });
+
+        $routeProvider.when('/messages', {
+            templateUrl: 'templates/chat/messages.html',
+            controller: 'MessagesController'
         });
     });
 
